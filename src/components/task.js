b7e754f87d4a9695312bf848ac4ff89344b9df7c
jrice <58012439+Justin-Rice@users.noreply.github.com>
2021-09-22 16:42:22 -0400

    added state

--- src/components/task.js
+++ src/components/task.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {List, Grid, Label, Button } from 'semantic-ui-react';
 
-const Task = ({name, color}) =>{
+const Task = ({name, color, editTask, index}) =>{
+
+    function editCurrentTask(){
+        editTask(index);
+
+    }
     return(
         <React.Fragment>
             <List.Item>
@@ -10,8 +15,19 @@ const Task = ({name, color}) =>{
                         <Label size='big' color={color}>{name}</Label>
                     </Grid.Column>
                     <Grid.Column width="6" textAlign='right'>
-                        <Button icon='trash' color='red'></Button>
-                        <Button icon='pencil' color='orange'></Button>
+                        <Button
+                         icon='trash' 
+                         color='red'
+                         >
+
+                         </Button>
+                        <Button 
+                        icon='pencil'
+                        color='orange'
+                        onClick={editCurrentTask}
+                         >
+
+                         </Button>
                      </Grid.Column>
                 </Grid>
            </List.Item>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,59 +2,121 @@ import React from 'react'
 import 'semantic-ui-css/semantic.css';
 import { Header, Container, Button, Grid, Segment,Form, Select, List, Label } from 'semantic-ui-react';
 import Task from '../components/task';
+import NewTaskForm from '../components/NewTaskForm';
 
 
-const  HomePage = () =>{
+const  HomePage = ({closeNewTask}) =>{
+  const initialNewTask = {
+    name:'',
+    color:'',
+  };
+
+  const [newTaskOpen, setNewTaskOpen] = React.useState(false);
+  const [newTask, setNewTask] = React.useState(initialNewTask);
+  const [list, setList] = React.useState([]);
+
+  function openNewTask(){
+    setNewTaskOpen(true)  
+}
+  function closeNewTask(){
+    setNewTaskOpen(false)  
+  }
+  function showTaskOpen(){
+    console.log(newTaskOpen);
+  }
+  function showNewTaskOpen(){
+    console.log(newTaskOpen);
+
+  }
+  function addNewTask(){
+    const listClone = [...list];
+    listClone.push(newTask);
+    setNewTask(initialNewTask);
+    setList(listClone);
+    closeNewTask();
+  }
+
+  //loop through list itmes or something
+  // const taskList = [];
+  // list.forEach((task, index) =>{
+  //   taskList.push(
+  //   <Task 
+  //     key={`${task.name}-${index}`} 
+  //     name={task.name} 
+  //     color={task.color}
+  //   />
+  //   );
+  //   console.log(taskList);
+
+  // });
+  function editTask(index){
+    console.log('edit', index)
+    const newList = list.map((task, i)=>{
+      if(i !== index) return task;
+      return {
+        name: `Edit ${task.name}`,
+        color : task.color,
+      }
+
+    })
+    setList(newList);
+  }
+
+  const taskList = list.map((task,index) => {
+    return(
+      <Task 
+      key={`${task.name}-${index}`} 
+      name={task.name} 
+      color={task.color}
+      editTask={editTask}
+      index={index}
+    />  
+    )
+  })
+
+  function deletTask(){
+    //Array.filter
+  }
+  
+
+
   return (
     <React.Fragment>
       <Container>
         <Grid columns='3'>
           <Grid.Column width='4'>
-            <Button icon='bars'></Button>
+            <Button 
+            icon='bars'
+            onClick={showTaskOpen}
+
+            ></Button>
           </Grid.Column>
           <Grid.Column width='8'>
             <Header textAlign="center" as='h1'>Todo List</Header>
           </Grid.Column>
           <Grid.Column textAlign='right' width='4'>
-            <Button icon='plus' color='green'></Button>
+            <Button 
+            icon='plus' 
+            color='green'
+            onClick={openNewTask}
+            ></Button>
           </Grid.Column>
         </Grid>
-        <Segment>
-          <Header as="h2">
-            New Task
-          </Header>
-          <Form>
-            <Form.Field
-            control='input'
-            label='Task Name'
-            placeholder='Enter task name...'
-            />
-            <Form.Field
-            control={Select}
-            label='Task Color'
-            placeholder='Choose task color...'
-            options={[
-              {text: 'Red', value : 'red'},
-              {text: 'Yellow', value : 'yellow'},
-              {text: 'Green', value : 'green'},
-
-
-            ]}
-            />
-            <Button.Group fluid>
-            <Button color='red'>Cancel</Button>
-            <Button.Or/>
-            <Button color='green'>Add Task</Button>
-            </Button.Group>
-
-          </Form>
-
-        </Segment>
+        {newTaskOpen ? 
+        (
+          <NewTaskForm 
+          closeNewTask= {closeNewTask}
+          newTask= {newTask}
+          setNewTask= {setNewTask}
+          addNewTask={addNewTask}
+          />
+        ) : null
+        
+        }
+        
         <List>
-            <Task name='Task 1' color="yellow"></Task>
-            <Task name='Task 2' color="red"></Task>
-            <Task name='Task 3' color="green"/>
-
+            {taskList}
+            
         </List>
       </Container>
     </React.Fragment>
